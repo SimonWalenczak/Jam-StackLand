25e97a6857404eebe2a621b01575a2304e248b64
P3D\simon.walenczak <simon.walenczak@lacatholille.fr>
2024-05-15 11:14:44 +0200

    Inital Commit

---
+++ Assets/Scripts/Data/ElementData.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "New Element", order = 1)]
+public class ElementData : ScriptableObject
+{
+    [field: SerializeField] public List<ElementData> ElementsRequieres;
+}
